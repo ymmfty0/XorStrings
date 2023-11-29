fa6d78fa5f15603519b65e57a4d46ad081645d81
ymmfty0 <114991711+ymmfty0@users.noreply.github.com>
2023-11-29 13:13:57 +0600

    Добавьте файлы проекта.

---
+++ XorStrings/XorStrings.cpp
@@ -0,0 +1,43 @@
+﻿#include <Windows.h>
+#include <string>
+#include <cstdio>
+
+#define KEY 0x1336
+
+template <unsigned int N>
+struct Xoring {
+
+	constexpr Xoring(const char* data) {
+
+		for (size_t i = 0; i < N; ++i) {
+			enc_[i] = data[i] ^ KEY;
+			str_[i] = data[i];
+		}
+	}
+
+	constexpr const char* dec() const
+	{
+		return str_;
+	}
+	constexpr const char* enc() const
+	{
+
+		return enc_;
+	}
+
+protected:
+	char str_[N] = { 0 };
+	char enc_[N] = { 0 };
+
+};
+
+int main()
+{
+
+	constexpr auto obfuscated_str = Xoring<6>("Ymmfty0");
+	const auto name = "Ymmfty1";
+	printf("%s\n", obfuscated_str.enc());
+	printf("%s\n", obfuscated_str.dec());
+
+}
+
